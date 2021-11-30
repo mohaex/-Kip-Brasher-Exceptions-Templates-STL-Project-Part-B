be5e73406aa25c0e162cf7dabc42bcd558656da6
Kip Brasher <kasey.brasher@sinclair.edu>
2021-11-29 19:54:19 -0500

    Set up some stuff

--- Kip Brasher Exceptions, Templates, STL Project 2b/Source.cpp
+++ Kip Brasher Exceptions, Templates, STL Project 2b/Source.cpp
@@ -1,14 +1,38 @@
 /*
 Name: Kip/Kasey Brasher
 Date: 11/29/2021
-Class: CIS 1202
+Course: CIS 1202
 */
 
+
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-
-int main()
+template <class K>
+K half(K number)
 {
+   
+}
 
+int half(int intValue)
+{
+    
+}
+
+int main() 
+{
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    int intValue;
+
+   
+    /*
+    Ref:
+    3.5
+    2.5
+    2
+    */
 }
